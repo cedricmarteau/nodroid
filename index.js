3ae34baccfa399b74c27ad02a078821ef169a61d
cedricmarteau <cedric.marteau@gmail.com>
2015-02-18 12:01:06 +0100

    socket io

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var http = require('http'),
-  express = require('express'),
-  app = express(),
-  server = require('http').createServer(app),
-  port = process.env.PORT || 5000,
-  call = {};
-  call.sound = true;
- 
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = server = require('http').createServer(app);
+var port = process.env.PORT || 5000;
+var io = require('socket.io')(server);
+
 app.use(express.bodyParser());
 
-app.get('/', function(request, response) {
-  response.sendfile('public/index.html');
+app.get('/', function(req, res){
+  res.sendfile('public/index.html');
 });
 
-app.post('/gestures',function(request,response){
-  console.log('New gesture' + request.body.action);
-  response.json({success: true, actionReceived: request.body.action});
-});
- 
 app.get(/^(.+)$/, function(req, res) {
   res.sendfile('public/' + req.params[0]);
 });
- 
-server.listen(port, function() {
-  console.log('Listening on ' + port);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('HandPosition', function(data){
+    console.log('HandPosition: ' + data);
+    io.emit('HandPosition', data);
+  });
+});
+
+server.listen(port, function(){
+  console.log('listening on *:5000');
 });
